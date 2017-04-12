7c1a9076fa6e25161843a644810ae054f66d260a
Seandor <seandor123@gmail.com>
2017-04-12 20:12:21 +0800

    add geocode module

--- udemy-nodejs/weather-app/app.js
+++ udemy-nodejs/weather-app/app.js
@@ -1,14 +1,24 @@
-const request = require('request');
+const yargs = require('yargs');
 
-request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20langbard%20street%20philadelphia',
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-    }
+const geocode = require('./geocode/geocode');
+
+const argv = yargs
+	.options({
+		a: {
+			demand: true,
+			alias: 'address',
+			describe: 'Adress to fetch weather for',
+			string: true
+		}
+	})
+	.help()
+	.alias('help', 'h')
+	.argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+	if (errorMessage) {
+		console.log(errorMessage);
+	} else {
+		console.log(JSON.stringify(results, undefined, 2));
+	}
 });
