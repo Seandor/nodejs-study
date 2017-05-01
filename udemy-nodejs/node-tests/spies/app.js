d6140be0591b89e8e6941a6db318e75ee582b6d3
Seandor <seandor123@gmail.com>
2017-05-01 18:12:43 +0800

    add spies

---
+++ udemy-nodejs/node-tests/spies/app.js
@@ -0,0 +1,11 @@
+var db = require('./db.js');
+
+module.exports.handleSignup = (email, password) => {
+  // check if the email already exists
+  db.saveUser({
+    email,
+    password
+  });
+
+  // send the welcome email
+}
